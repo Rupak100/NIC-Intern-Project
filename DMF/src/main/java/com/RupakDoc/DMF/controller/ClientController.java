96a8b1c22b1882ebd273c11bc7e196073251676b
Rupak100 <112826831+Rupak100@users.noreply.github.com>
2024-06-25 17:35:58 +0530

    last commit

---
+++ DMF/src/main/java/com/RupakDoc/DMF/controller/ClientController.java
@@ -0,0 +1,35 @@
+package com.RupakDoc.DMF.controller;
+
+
+import com.RupakDoc.DMF.service.ClientService;
+import com.RupakDoc.DMF.dto.SignUpRequestDto;
+import com.RupakDoc.DMF.model.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserEntity> signup(@RequestParam String clientId, @RequestBody SignUpRequestDto signUpRequestDto) {
+        return clientService.signup(clientId, signUpRequestDto)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.badRequest().build());
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestParam long mobileNo) {
+        Optional<String> otp = clientService.login(mobileNo);
+        return otp.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.badRequest().body("Invalid mobile number"));
+    }
+}
